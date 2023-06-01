743b9bdc842329d9f6ffe373f7f7c52ada678577
azkifairuz <azkiajmal@gmai.com>
2023-06-02 01:42:24 +0700

    card title

--- app/src/main/java/com/azkifairuz/myapplication/ui/home/home.kt
+++ app/src/main/java/com/azkifairuz/myapplication/ui/home/home.kt
@@ -3,15 +3,19 @@ package com.azkifairuz.myapplication.ui.home
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.paging.compose.items
@@ -46,7 +50,19 @@ fun NewsCard(news: NewsItem) {
             .fillMaxSize()
     ){
         Column() {
-            Image(painter = image, contentDescription = null)
+            Image(
+                painter = image, 
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .height(250.dp)
+            )
+            Column(modifier = Modifier.padding(10.dp)) {
+                
+                Text(text = news.title, fontWeight = FontWeight.Bold)
+                
+            }
         }
     }
 }
