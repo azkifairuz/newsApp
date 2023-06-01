938f210e12c6943c1c56dd1bdf4c30e2b27a2d25
azkifairuz <azkiajmal@gmai.com>
2023-06-01 22:48:00 +0700

    constant for endpoint

--- app/src/main/java/com/azkifairuz/myapplication/utils/Constant.kt
+++ app/src/main/java/com/azkifairuz/myapplication/utils/Constant.kt
@@ -3,5 +3,7 @@ package com.azkifairuz.myapplication.utils
 class Constant {
     companion object {
         const val BASE_URL = "https://newsapi.org/v2/"
+        const val TOP_HEADLINE_ENDPOINT = "top-headlines"
+        const val EVERYTHING_ENDPOINT = "everything"
     }
 }
