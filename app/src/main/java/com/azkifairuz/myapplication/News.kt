9d3ee4c2e499466037aba570ec4ec926a5f7cb04
azkifairuz <azkiajmal@gmai.com>
2023-06-01 17:53:57 +0700

    create data class

---
+++ app/src/main/java/com/azkifairuz/myapplication/News.kt
@@ -0,0 +1,13 @@
+package com.azkifairuz.myapplication
+
+import java.util.Date
+
+
+data class News(
+    val title: String,
+    val description: String,
+    val urlToImage: String,
+    val author: String,
+    val date: Date,
+
+)
