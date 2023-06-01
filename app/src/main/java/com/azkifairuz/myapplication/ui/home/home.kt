bc20057b2901a3bbf9e98aea298a67ad8a43a1b1
azkifairuz <azkiajmal@gmai.com>
2023-06-02 01:30:12 +0700

    create lazy colmn to show news in lst

--- app/src/main/java/com/azkifairuz/myapplication/ui/home/home.kt
+++ app/src/main/java/com/azkifairuz/myapplication/ui/home/home.kt
@@ -1,8 +1,26 @@
 package com.azkifairuz.myapplication.ui.home
 
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun HomeScreen() {
+    
+    val homeViewModel = viewModel(modelClass = HomeViewModel::class.java)
+    val news by homeViewModel.news.collectAsState()
+    
+    LazyColumn{
+        item (news) {news:newsItem->
+            
+            NewsCard(news = news)
+        }
+    }
+}
+
+@Composable
+fun NewsCard(news: Any) {
 
 }
